2ccfe39d20e17c9069fa42f227e54e23205ce3f5
sangam-pawar <76432047+sangam-pawar@users.noreply.github.com>
2024-07-08 13:59:37 +0530

    Add files via upload

---
+++ whileloop.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     /*
+     int n;
+     cin>>n;
+
+     int i =1;
+
+
+     while (i<=n){
+     
+        cout << i<< "" << endl;
+        i = i+1;
+     }
+     */
+
+    int n ;
+    cin >>n;
+
+    int i=1;
+    int sum = 0;
+     
+    while (i<=n){
+        
+        sum = sum +i;
+        i=i+1;
+    }
+
+    cout<< "value of sum is:" <<sum<<endl;
+
+
+}
